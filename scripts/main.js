e12eadb63185d40ec6c33eba46f6b00e92b0819e
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-10-11 14:54:24 +0200

    add week 4 class content

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+$(document).readyState(function() {
+
+
+
+
+
+    // -------------------------------
+    // on submit prevent the default form submission 
+
+    $('#signupForm').submit(function(event) {
+
+        event.preventDefault();
+
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else{
+            // Add any submission code here, like saving the data to localStorage
+            window.location.href = 'pages/browse.html';
+        }
+        $(this).addClass('was-validated');
+
+    });
+});
