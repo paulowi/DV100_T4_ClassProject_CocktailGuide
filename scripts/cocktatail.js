3e3bd08b8b8190cdb34b70e54bb90df9e27f45b1
Onthatile-Lesufi <231262@virtualwindow.co.za>
2023-10-18 16:30:00 +0200

    Week 5

--- scripts/cocktatail.js
+++ scripts/cocktatail.js
@@ -21,6 +21,15 @@ getCocktailDetails = (cocktailID) => {
             const cocktail = data.drinks[0];
 
             console.log(cocktail);
+            $('#cocktailName').text(cocktail.strDrink);
+            $('#cocktailCategory').text(cocktail.strCategory);
+            $('#cocktailGlass').text(cocktail.strGlass);
+            $('#cocktailInstructions').text(cocktail.strInstructions);
+            $('#cocktailImage').attr('src', cocktail.strDrinkThumb);
+            $('#cocktailIngredient1').text(cocktail.strIngredient1);
+            $('#cocktailIngredient2').text(cocktail.strIngredient2);
+            $('#cocktailIngredient3').text(cocktail.strIngredient3);
+            $('#cocktailIngredient4').text(cocktail.strIngredient4);
         },
         error: function(data){
             // Run Error
