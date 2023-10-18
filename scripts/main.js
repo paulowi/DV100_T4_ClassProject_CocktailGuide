88f6dda9e498b3ce9a7a75a5915257dc43a15761
Sean-D-231121 <231121@virtualwindow.co.za>
2023-10-18 09:04:00 +0200

    Added password validation
    Added password validation which checks for at least 1 number, lowercase letter and uppercase letter. It also has be at least 6 characters long and the entered password and confirm password.

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,38 @@ $(document).ready(function() {
 
     // ---------------------------------------------------------------------------------------------------
     // On Submit, prevent the default form submission
-
+    $(this).find(".password-invalid").hide();
     $('#signupForm').submit(function(event) {
-
         event.preventDefault();
-        
         if (this.checkValidity() === false) {
             event.stopPropagation();
-        } else {
-            // Add any submission code here, like saving the data to localStorage
-            window.location.href = 'pages/browse.html';
+        }
+        else if (this.checkValidity() === true && $("#password").val() === $("#confirm-password").val()) {
+             // Add any submission code here, like saving the data to localStorage
+            
+             window.location.href = "pages/browse.html"; 
+             $(this).find(".password-invalid").hide();
+        }
+        else{
+           
+           event.stopPropagation();
+             $(this).find("#confirm-password").addClass("password-problem");
+             $(this).find(".password-invalid").show();
+              showPasswordError("#confirm-password");
+              
         }
         $(this).addClass('was-validated');
+        
     });
 });
+showPasswordError = (passwordId) => {
+    $(passwordId).keyup(function(){
+        if ($("#password").val() !== $("#confirm-password").val()){
+          $(passwordId).addClass("password-problem");
+        $(".password-invalid").show();
+        } else {
+            $(passwordId).removeClass("password-problem");
+            $(".password-invalid").hide();
+        }
+    })
+}
